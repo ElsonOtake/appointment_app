ff626af0034564abe0c8ce923b68a70b26f2d827
Elson Otake <elsonotake@yahoo.com.br>
2023-03-08 11:33:49 -0300

    Add stream to delete appointment

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -51,7 +51,7 @@ class AppointmentsController < ApplicationController
 
     respond_to do |format|
       format.html { redirect_to appointments_url, notice: 'Appointment was successfully destroyed.' }
-      format.json { head :no_content }
+      format.turbo_stream
     end
   end
 
