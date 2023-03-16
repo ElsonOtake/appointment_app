2d60454a87a519fa7770a3be1a27ea0900625ee5
Elson Otake <elsonotake@yahoo.com.br>
2023-03-15 21:31:04 -0300

    Add broadcast

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,4 +2,6 @@ class Appointment < ApplicationRecord
   validates :booking, presence: true
 
   scope :ordered, -> { order(id: :desc) }
+
+  broadcasts_to ->(appointment) { 'appointments' }, inserts_by: :prepend
 end
